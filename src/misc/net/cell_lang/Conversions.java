ac2cf993c62ce3297ce3740d552bc104a1dd4e64
cell-lang <cell.lang.dev@gmail.com>
2018-07-22 16:25:26 +0200

    MISCELLANEA BUG FIXES

--- src/misc/net/cell_lang/Conversions.java
+++ src/misc/net/cell_lang/Conversions.java
@@ -2,19 +2,18 @@ package net.cell_lang;
 
 
 class Conversions {
-  // public static Obj convertText(String text) {
-  //   int len = text.length();
-  //   byte[] bytes = new byte[len];
-  //   for (int i=0 ; i < len ; i++) {
-  //     char ch = text.charAt(i);
-  //     if (ch > 255)
-  //       throw new ParsingException(i);
-  //     bytes[i] = (byte) ch;
-  //   }
+  public static Obj convertText(String text) {
+    int len = text.length();
+    byte[] bytes = new byte[len];
+    for (int i=0 ; i < len ; i++) {
+      char ch = text.charAt(i);
+      if (ch > 255)
+        throw new ParsingException(i);
+      bytes[i] = (byte) ch;
+    }
 
-  //   Token[] tokens = Lexer.lex(bytes);
-  //   return Parser.parse(tokens);
-  // }
+    return Generated.parse(bytes);
+  }
 
   public static Value exportAsValue(Obj obj) {
     return obj.getValue();
@@ -22,84 +21,48 @@ class Conversions {
 
   public static Obj stringToObj(String str) {
     int[] cps = Miscellanea.codePoints(str);
-    int len = cps.length;
-    Obj[] objs = new Obj[len];
-    for (int i=0 ; i < len ; i++)
-      objs[i] = IntObj.get(cps[i]);
-    return Builder.createTaggedObj(SymbTable.StringSymbId, new MasterSeqObj(objs));
+    Int32ArrayObj obj = IntArrayObjs.create(cps);
+    return Builder.createTaggedObj(SymbTable.StringSymbId, obj);
   }
 
   ////////////////////////////////////////////////////////////////////////////
 
   public static boolean[] toBoolArray(Obj obj) {
-    int size = obj.getSize();
-    boolean[] array = new boolean[size];
-    SeqOrSetIter it = obj.getSeqOrSetIter();
-    int idx = 0;
-    while (!it.done()) {
-      array[idx++] = it.get().getBool();
-      it.next();
-    }
-    return array;
+    return obj.getArray((boolean[]) null);
   }
 
   public static long[] toLongArray(Obj obj) {
-    int size = obj.getSize();
-    long[] array = new long[size];
-    SeqOrSetIter it = obj.getSeqOrSetIter();
-    int idx = 0;
-    while (!it.done()) {
-      array[idx++] = it.get().getLong();
-      it.next();
-    }
-    return array;
+    return obj.getArray((long[]) null);
   }
 
   public static double[] toDoubleArray(Obj obj) {
-    int size = obj.getSize();
-    double[] array = new double[size];
-    SeqOrSetIter it = obj.getSeqOrSetIter();
-    int idx = 0;
-    while (!it.done()) {
-      array[idx++] = it.get().getDouble();
-      it.next();
-    }
-    return array;
+    return obj.getArray((double[]) null);
   }
 
   public static String[] toSymbolArray(Obj obj) {
-    int size = obj.getSize();
-    String[] array = new String[size];
-    SeqOrSetIter it = obj.getSeqOrSetIter();
-    int idx = 0;
-    while (!it.done()) {
-      array[idx++] = it.get().toString();
-      it.next();
-    }
-    return array;
+    Obj[] elts = obj.getArray((Obj[]) null);
+    int len = elts.length;
+    String[] symbs = new String[len];
+    for (int i=0 ; i < len ; i++)
+      symbs[i] = elts[i].toString();
+    return symbs;
   }
 
   public static String[] toStringArray(Obj obj) {
-    int size = obj.getSize();
-    String[] array = new String[size];
-    SeqOrSetIter it = obj.getSeqOrSetIter();
-    int idx = 0;
-    while (!it.done()) {
-      array[idx++] = it.get().getString();
-      it.next();
-    }
-    return array;
+    Obj[] elts = obj.getArray((Obj[]) null);
+    int len = elts.length;
+    String[] strs = new String[len];
+    for (int i=0 ; i < len ; i++)
+      strs[i] = elts[i].getString();
+    return strs;
   }
 
   public static Value[] toValueArray(Obj obj) {
-    int size = obj.getSize();
-    Value[] array = new Value[size];
-    SeqOrSetIter it = obj.getSeqOrSetIter();
-    int idx = 0;
-    while (!it.done()) {
-      array[idx++] = it.get().getValue();
-      it.next();
-    }
-    return array;
+    Obj[] elts = obj.getArray((Obj[]) null);
+    int len = elts.length;
+    Value[] values = new Value[len];
+    for (int i=0 ; i < len ; i++)
+      values[i] = elts[i].getValue();
+    return values;
   }
 }
