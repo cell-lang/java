d136b6f3350f1539141474efbf01c6e27e38cc3d
cell-lang <cell.lang.dev@gmail.com>
2019-03-19 06:16:54 +0100

    REDUCED THE CAPACITY OF THE HASHTABLE

--- src/automata/net/cell_lang/ValueStore.java
+++ src/automata/net/cell_lang/ValueStore.java
@@ -7,10 +7,10 @@ class ValueStore {
   private Obj[] values             = new Obj[INIT_SIZE]; //           null
   private int[] hashcodeOrNextFree = new int[INIT_SIZE]; // hashcode  index of the next free slot (can be out of bound)
 
-  private byte[] references = new byte[INIT_SIZE];
-  private IntCtrs extraRefs = new IntCtrs();
+  private byte[]  references = new byte[INIT_SIZE];
+  private IntCtrs extraRefs  = new IntCtrs();
 
-  private int[] hashtable = new int[INIT_SIZE]; // -1 when there's no value in that bucket
+  private int[] hashtable = new int[INIT_SIZE/2]; // -1 when there's no value in that bucket
   private int[] buckets   = new int[INIT_SIZE]; // junk when there's no value
 
   private int count = 0;
@@ -27,29 +27,19 @@ class ValueStore {
   //////////////////////////////////////////////////////////////////////////////
 
   public void printInfo(String name) {
-  //   int usedBuckets = 0;
-  //   int largeRefCounts = 0;
-  //   int[] refCountHistogram = new int[16];
-  //   for (int i=0 ; i < values.length ; i++)
-  //     if (values[i] != null) {
-  //       if (buckets[i] != -1)
-  //         usedBuckets++;
-  //       if (references[i] > 255)
-  //         largeRefCounts++;
-  //       else
-  //         refCountHistogram[references[i]/16]++;
-  //     }
-  //   // System.out.printf("%-16s: %d/%d (%f)\n", name + ":", usedBuckets, count, (double) usedBuckets / (double) count);
-  //   // System.out.printf("%-16s %6d/%7d (%f)\n", name + ":", largeRefCounts, count, (double) largeRefCounts / (double) count);
-  //   System.out.printf("%-14s %7d", name + ":", count);
-  //   int total = 0;
-  //   for (int i=0 ; i < 16 ; i++) {
-  //     // System.out.printf(" %7d", refCountHistogram[i]);
-  //     // System.out.printf(" %4f", refCountHistogram[i] / (double) count);
-  //     total += refCountHistogram[i];
-  //     System.out.printf(" %.4f", total / (double) count);
-  //   }
-  //   System.out.println();
+    // int usedBuckets = 0;
+    // for (int i=0 ; i < values.length ; i++)
+    //   if (values[i] != null)
+    //     if (buckets[i] != -1)
+    //       usedBuckets++;
+    // System.out.printf(
+    //   "%-14s: %6d / %7d (%f) - %f\n",
+    //   name + ":",
+    //   usedBuckets,
+    //   count,
+    //   (double) usedBuckets / (double) count,
+    //   (double) count / (double) values.length
+    // );
   }
 
   public void insert(Obj value, int hashcode, int index) {
@@ -126,7 +116,7 @@ class ValueStore {
 
     values             = new Obj[newCapacity];
     hashcodeOrNextFree = new int[newCapacity];
-    hashtable          = new int[newCapacity];
+    hashtable          = new int[newCapacity/2];
     buckets            = new int[newCapacity];
     references         = new byte[newCapacity];
 
@@ -184,14 +174,14 @@ class ValueStore {
   //////////////////////////////////////////////////////////////////////////////
 
   private void insertIntoHashtable(int index, int hashcode) {
-    int hashIdx = Integer.remainderUnsigned(hashcode, values.length);
+    int hashIdx = Integer.remainderUnsigned(hashcode, hashtable.length);
     buckets[index] = hashtable[hashIdx];
     hashtable[hashIdx] = index;
   }
 
   private void removeFromHashtable(int index) {
     int hashcode = hashcodeOrNextFree[index];
-    int hashIdx = Integer.remainderUnsigned(hashcode, values.length);
+    int hashIdx = Integer.remainderUnsigned(hashcode, hashtable.length);
     int idx = hashtable[hashIdx];
     Miscellanea._assert(idx != -1);
 
