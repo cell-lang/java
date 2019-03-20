fe4dbf38b01b0af07efa34a2686c2f04973a31d9
cell-lang <cell.lang.dev@gmail.com>
2019-03-20 14:52:25 +0100

    TYPE-SPECIFIC VALUE STORES #1

---
+++ src/automata/net/cell_lang/IntStore.java
@@ -0,0 +1,213 @@
+package net.cell_lang;
+
+
+class IntStore {
+  static final int INIT_SIZE = 256;
+
+  // Bits  0 - 31: 32-bit value, or index of 64-bit value
+  // Bits 32 - 60: index of next value in the bucket if used or next free index otherwise
+  // Bits 61 - 63: tag: 000 used (32 bit), 001 used (64 bit), 002 free
+  private long[] slots = new long[INIT_SIZE];
+
+  private byte[]  references = new byte[INIT_SIZE];
+  private IntCtrs extraRefs  = new IntCtrs();
+
+  private int[] hashtable = new int[INIT_SIZE/2]; // -1 when there's no value in that bucket
+
+  private int count = 0;
+  private int firstFree = 0;
+
+  //////////////////////////////////////////////////////////////////////////////
+
+  private int hashIdx(long value) {
+    int hashcode = (int) (value ^ (value >> 32));
+    return Integer.remainderUnsigned(hashcode, hashtable.length);
+  }
+
+  private long emptySlot(int next) {
+    return (((long) next) | (2L << 29)) << 32;
+  }
+
+  private long filledSlot(long value, int next) {
+    if (value != ((int) value))
+      throw new RuntimeException();
+    return value | (((long) next) << 32);
+  }
+
+  private long value(long slot) {
+    Miscellanea._assert(!isEmpty(slot));
+    return (int) slot;
+  }
+
+  private int next(long slot) {
+    Miscellanea._assert(!isEmpty(slot));
+    return (int) (slot >> 32);
+  }
+
+  private int nextFree(long slot) {
+    Miscellanea._assert(isEmpty(slot));
+    return (int) ((slot >> 32) & 0x1FFFFFFF);
+  }
+
+  private boolean isEmpty(long slot) {
+    long tag = slot >> 61;
+    Miscellanea._assert(tag == 0 | tag == 2);
+    return tag == 2;
+  }
+
+  //////////////////////////////////////////////////////////////////////////////
+
+  public void addRef(int index) {
+    int refs = Byte.toUnsignedInt(references[index]) + 1;
+    if (refs == 256) {
+      extraRefs.increment(index);
+      refs -= 64;
+    }
+    references[index] = (byte) refs;
+  }
+
+  public void release(int index) {
+    Miscellanea._assert(references[index] > 0);
+    Miscellanea._assert(!isEmpty(slots[index]));
+
+    int refs = Byte.toUnsignedInt(references[index]) - 1;
+    if (refs == 127) {
+      if (extraRefs.tryDecrement(index))
+        refs += 64;
+    }
+    else if (refs == 0) {
+      free(index);
+    }
+    references[index] = (byte) refs;
+  }
+
+  //////////////////////////////////////////////////////////////////////////////
+
+  public IntStore() {
+    for (int i=0 ; i < INIT_SIZE ; i++)
+      slots[i] = emptySlot(i+1);
+    Miscellanea.arrayFill(hashtable, -1);
+  }
+
+  //////////////////////////////////////////////////////////////////////////////
+
+  public void insert(long value, int index) {
+    Miscellanea._assert(firstFree == index);
+    Miscellanea._assert(index < slots.length);
+    Miscellanea._assert(references[index] == 0);
+
+    count++;
+    firstFree = nextFree(slots[index]);
+
+    int hashIdx = hashIdx(value);
+    slots[index] = filledSlot(value, hashtable[hashIdx]);
+    hashtable[hashIdx] = index;
+  }
+
+  public int insertOrAddRef(long value) {
+    int surr = valueToSurr(value);
+    if (surr != -1) {
+      addRef(surr);
+      return surr;
+    }
+    else {
+      Miscellanea._assert(count <= capacity());
+      if (count == capacity())
+        resize(count + 1);
+      int idx = firstFree;
+      insert(value, idx);
+      return idx;
+    }
+  }
+
+  public void resize(int minCapacity) {
+    int currCapacity = capacity();
+    int newCapacity = 2 * currCapacity;
+    while (newCapacity < minCapacity)
+      newCapacity = 2 * newCapacity;
+
+    long[] currSlots      = slots;
+    byte[] currReferences = references;
+
+    slots      = new long[newCapacity];
+    references = new byte[newCapacity];
+    hashtable  = new int[newCapacity/2];
+
+    Miscellanea.arrayCopy(currReferences, references, currCapacity);
+    Miscellanea.arrayFill(hashtable, -1);
+
+    count = 0;
+    for (int i=0 ; i < currCapacity ; i++) {
+      long slot = currSlots[i];
+      Miscellanea._assert(!isEmpty(slot));
+      insert(value(slot), i);
+    }
+
+    for (int i=currCapacity ; i < newCapacity ; i++)
+      slots[i] = emptySlot(i+1);
+  }
+
+  //////////////////////////////////////////////////////////////////////////////
+
+  public int count() {
+    return count;
+  }
+
+  public int capacity() {
+    return slots.length;
+  }
+
+  public int nextFreeIdx(int index) {
+    Miscellanea._assert(index == -1 || index >= capacity() || isEmpty(slots[index]));
+    if (index == -1)
+      return firstFree;
+    if (index >= capacity())
+      return index + 1;
+    return nextFree(slots[index]);
+  }
+
+  public int valueToSurr(long value) {
+    int hashIdx = hashIdx(value);
+    int idx = hashtable[hashIdx];
+    while (idx != -1) {
+      long slot = slots[idx];
+      if (value(slot) == value)
+        return idx;
+      idx = next(slot);
+    }
+    return -1;
+  }
+
+  public long surrToValue(int index) {
+    return value(slots[index]);
+  }
+
+  //////////////////////////////////////////////////////////////////////////////
+
+  private void free(int index) {
+    long slot = slots[index];
+    int hashIdx = hashIdx(value(slot));
+
+    int idx = hashtable[hashIdx];
+    Miscellanea._assert(idx != -1);
+
+    if (idx == index) {
+      hashtable[hashIdx] = next(slot);
+    }
+    else {
+      for ( ; ; ) {
+        slot = slots[idx];
+        int next = next(slot);
+        if (next == index) {
+          slots[idx] = filledSlot(value(slot), next(slots[next]));
+          break;
+        }
+        idx = next;
+      }
+    }
+
+    slots[index] = emptySlot(firstFree);
+    firstFree = index;
+    count--;
+  }
+}
---
+++ src/automata/net/cell_lang/IntStoreUpdater.java
@@ -0,0 +1,151 @@
+package net.cell_lang;
+
+
+class IntStoreUpdater {
+  static final int INIT_SIZE = 32;
+
+  private long[] values     = new long[INIT_SIZE];
+  private int[]  surrogates = new int[INIT_SIZE];
+
+  private int[] hashtable  = new int[INIT_SIZE];
+  private int[] buckets    = new int[INIT_SIZE];
+
+  private int count = 0;
+  private int hashRange = 0;
+  private int lastSurrogate = -1;
+
+  private IntStore store;
+
+  //////////////////////////////////////////////////////////////////////////////
+
+  public IntStoreUpdater(IntStore store) {
+    Miscellanea.arrayFill(hashtable, -1);
+    this.store = store;
+  }
+
+  public void apply() {
+    if (count == 0)
+      return;
+
+    int storeCapacity = store.capacity();
+    int reqCapacity = store.count() + count;
+
+    if (storeCapacity < reqCapacity)
+      store.resize(reqCapacity);
+
+    for (int i=0 ; i < count ; i++)
+      store.insert(values[i], surrogates[i]);
+  }
+
+  public void reset() {
+    if (hashRange != 0)
+      Miscellanea.arrayFill(hashtable, hashRange, -1);
+
+    count = 0;
+    hashRange = 0;
+    lastSurrogate = -1;
+  }
+
+  public int lookupOrInsertValue(long value) {
+    int surr = valueToSurr(value);
+    if (surr != -1)
+      return surr;
+    return insert(value);
+  }
+
+  // Inefficient, but used only for debugging
+  public long surrToValue(int surr) {
+    for (int i=0 ; i < count ; i++)
+      if (surrogates[i] == surr)
+        return values[i];
+    return store.surrToValue(surr);
+  }
+
+  //////////////////////////////////////////////////////////////////////////////
+
+  private int hashcode(long value) {
+    return (int) (value ^ (value >> 32));
+  }
+
+  private int insert(long value) {
+    Miscellanea._assert(count <= values.length);
+
+    lastSurrogate = store.nextFreeIdx(lastSurrogate);
+    int hashcode = hashcode(value);
+
+    if (count == values.length)
+      resize();
+
+    values[count]     = value;
+    surrogates[count] = lastSurrogate;
+
+    if (count >= 16) {
+      if (count >= hashRange) {
+        if (hashRange != 0) {
+          Miscellanea.arrayFill(hashtable, hashRange, -1);
+          hashRange *= 2;
+        }
+        else
+          hashRange = 16;
+
+        for (int i=0 ; i < count ; i++)
+          insertIntoHashtable(i, hashcode(values[i]));
+      }
+      insertIntoHashtable(count, hashcode);
+    }
+    count++;
+
+    return lastSurrogate;
+  }
+
+  private int valueToSurr(long value) {
+    int surrogate = store.valueToSurr(value);
+    if (surrogate != -1)
+      return surrogate;
+
+    if (count > 0) {
+      if (hashRange == 0) {
+        for (int i=0 ; i < count ; i++)
+          if (values[i] == value)
+            return surrogates[i];
+      }
+      else {
+        int hashIdx = Integer.remainderUnsigned(hashcode(value), hashRange);
+        for (int i = hashtable[hashIdx] ; i != -1 ; i = buckets[i])
+          if (values[i] == value)
+            return surrogates[i];
+      }
+    }
+
+    return -1;
+  }
+
+  private void resize() {
+    Miscellanea._assert(hashRange == values.length);
+
+    int currCapacity = values.length;
+    int newCapacity = 2 * currCapacity;
+
+    long[] currValues     = values;
+    int[]  currSurrogates = surrogates;
+
+    values     = new long[newCapacity];
+    hashtable  = new int[newCapacity];
+    buckets    = new int[newCapacity];
+    surrogates = new int[newCapacity];
+    hashRange  = newCapacity;
+
+    Miscellanea.arrayCopy(currValues, values, currCapacity);
+    Miscellanea.arrayCopy(currSurrogates, surrogates, currCapacity);
+    Miscellanea.arrayFill(hashtable, -1);
+
+    for (int i=0 ; i < count ; i++)
+      insertIntoHashtable(i, hashcode(values[i]));
+  }
+
+  private void insertIntoHashtable(int index, int hashcode) {
+    int hashIdx = Integer.remainderUnsigned(hashcode, hashRange);
+    buckets[index] = hashtable[hashIdx];
+    hashtable[hashIdx] = index;
+  }
+}
