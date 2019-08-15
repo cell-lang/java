9f9bb593c651f04f0baf7e7f73761088b0e7474d
cell-lang <cell.lang.dev@gmail.com>
2019-08-15 17:55:27 +0200

    ADDED Date <-> LocalDate AND Time <-> LocalDateTime INTERFACE MAPPINGS

--- src/utils/net/cell_lang/Conversions.java
+++ src/utils/net/cell_lang/Conversions.java
@@ -2,6 +2,9 @@ package net.cell_lang;
 
 import java.util.Arrays;
 import java.io.StringReader;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
 
 
 class Conversions {
@@ -21,6 +24,51 @@ class Conversions {
     //## THIS ONE IS REAL BAD TOO. IT SHOULD USE THE MINIMUM SIZE ARRAY POSSIBLE!
     int[] cps = Miscellanea.codePoints(str);
     return Builder.createTaggedObj(SymbTable.StringSymbId, Builder.createSeq(cps));
+  }
+
+  ////////////////////////////////////////////////////////////////////////////
+
+  // java.time.LocalDate.ofEpochDay(long epochDay)
+  //   long toEpochDay()
+
+  public static Obj dateToObj(LocalDate date) {
+    return Builder.createTaggedIntObj(SymbTable.DateSymbId, date.toEpochDay());
+  }
+
+  public static LocalDate objToDate(Obj date) {
+    long epochNanoSecs = date.getInnerLong();
+    long epochDay = epochNanoSecs >= 0 ?
+      epochNanoSecs / 86400000000000L :
+      ((epochNanoSecs + 86399999999999L) / 86400000000000L) - 1;
+    return LocalDate.ofEpochDay(epochDay);
+  }
+
+  // java.time.LocalDateTime.ofEpochSecond(long epochSecond, int nanoOfSecond, ZoneOffset.UTC)
+  //   long toEpochSecond(ZoneOffset offset)
+  //   int getNano()
+
+  public static Obj dateTimeToObj(LocalDateTime time) {
+    long epochSecond = time.toEpochSecond(ZoneOffset.UTC);
+    int nanosecs = time.getNano();
+    if (epochSecond >= -9223372036L) {
+      if (epochSecond < 9223372036L | (epochSecond == 9223372036L & nanosecs <= 854775807)) {
+        long epochNanoSecs = 1000000000 * epochSecond + nanosecs;
+        return Builder.createTaggedIntObj(SymbTable.TimeSymbId, epochNanoSecs);
+      }
+    }
+    else if (epochSecond == -9223372037L & nanosecs >= 145224192) {
+      long epochNanoSecs = -9223372036000000000L - (1000000000 - nanosecs);
+      return Builder.createTaggedIntObj(SymbTable.TimeSymbId, epochNanoSecs);      
+    }
+    throw new RuntimeException("DateTime is outside the supported range: " + time.toString());
+  }
+
+  public static LocalDateTime objToDateTime(Obj time) {
+    long epochNanoSecs = time.getInnerLong();
+    long epochSecond = epochNanoSecs >= 0 ?
+      epochNanoSecs / 1000000000 :
+      ((epochNanoSecs + 999999999) / 1000000000) - 1;
+    return LocalDateTime.ofEpochSecond(epochSecond, (int) (epochNanoSecs - epochSecond), ZoneOffset.UTC);
   }
 
   ////////////////////////////////////////////////////////////////////////////
